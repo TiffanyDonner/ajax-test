cbdd05a58c5b7e743b010253b62c9f899fd0caf1
Tiffany Donner <tiffany.donner@stsupport.se>
2020-01-29 15:40:37 +0000

    JSON.Parse() lesson material completed

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 var xhr = new XMLHttpRequest();
+var data;
+
+xhr.open("GET", "https://swapi.co/api/");
+xhr.send();
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+        data = this.responseText;
+    };
+}
 
-xhr.open("GET", "https://swapi.co/api/");
-
-xhr.send();
+console.log(data);
